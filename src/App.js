29577043405acdaa6d31725e5298bda74f342572
Denise <denisewaskow@gmail.com>
2019-11-04 12:26:35 +0000

    quest form-post

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FavoriteFilm from './FavoriteFilm.js';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <FavoriteFilm />
+    </>
+  );
+}
+
+export default App;
